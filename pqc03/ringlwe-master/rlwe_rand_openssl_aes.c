adf53e09e461043e83fa6f1049932656844440fc
gksdbwns <a01049777206@gmail.com>
2026-09-11 10:27:49 +0900

    rlwe_rand_openssl_aes.c 수정

--- pqc03/ringlwe-master/rlwe_rand_openssl_aes.c
+++ pqc03/ringlwe-master/rlwe_rand_openssl_aes.c
@@ -1,26 +1,32 @@
-#include <openssl/rand.h>
-#include <openssl/aes.h>
+#include <sys/random.h>
+#include <stdint.h>
+#include <stdlib.h>
 
-#define RANDOM_VARS \
-	AES_KEY aes_key; \
-	unsigned char aes_key_bytes[16]; \
-	RAND_bytes(aes_key_bytes, 16); \
-	AES_set_encrypt_key(aes_key_bytes, 128, &aes_key); \
-	unsigned char aes_ivec[AES_BLOCK_SIZE]; \
-	memset(aes_ivec, 0, AES_BLOCK_SIZE); \
-	unsigned char aes_ecount_buf[AES_BLOCK_SIZE]; \
-	memset(aes_ecount_buf, 0, AES_BLOCK_SIZE); \
-	unsigned int aes_num = 0; \
-	unsigned char aes_in[AES_BLOCK_SIZE]; \
-	memset(aes_in, 0, AES_BLOCK_SIZE);
-#define RANDOM8   ((uint8_t) randomplease(&aes_key, aes_ivec, aes_ecount_buf, &aes_num, aes_in))
-#define RANDOM32 ((uint32_t) randomplease(&aes_key, aes_ivec, aes_ecount_buf, &aes_num, aes_in))
-#define RANDOM64 ((uint64_t) randomplease(&aes_key, aes_ivec, aes_ecount_buf, &aes_num, aes_in))
+/* 
+ * 1. 상태 변수 제거
+ * 원래 OpenSSL AES 컨텍스트를 초기화하던 매크로입니다.
+ * 리눅스 getrandom()은 커널 레벨에서 상태를 관리하므로 앱 단의 상태 변수가 필요 없습니다.
+ * 따라서 에러가 나지 않도록 빈 매크로로 둡니다.
+ */
+#define RANDOM_VARS 
 
-uint64_t randomplease(AES_KEY *aes_key, unsigned char aes_ivec[AES_BLOCK_SIZE],
-                      unsigned char aes_ecount_buf[AES_BLOCK_SIZE],
-                      unsigned int *aes_num, unsigned char aes_in[AES_BLOCK_SIZE]) {
-	uint64_t out;
-	AES_ctr128_encrypt(aes_in, (unsigned char *) &out, 8, aes_key, aes_ivec, aes_ecount_buf, aes_num);
-	return out;
+/* 
+ * 2. 난수 생성 함수 (OpenSSL 대체)
+ */
+static inline uint64_t randomplease_linux(void) {
+    uint64_t out = 0;
+    // 리눅스 커널의 CSPRNG에서 직접 8바이트(64비트)를 가져옴
+    if (getrandom(&out, sizeof(out), 0) < 0) {
+        exit(1);
+    }
+    return out;
 }
+
+/* 
+ * 3. 매크로 덮어쓰기
+ * rlwe_kex.c에서 호출하는 매크로들이 기존의 복잡한 인자 대신 
+ * 새로 만든 randomplease_linux()를 호출하도록 연결합니다.
+ */
+#define RANDOM8  ((uint8_t) randomplease_linux())
+#define RANDOM32 ((uint32_t) randomplease_linux())
+#define RANDOM64 ((uint64_t) randomplease_linux())
